905a7601a25cc7c2e5c693bc1b1b1a4042d82aaa
rainverted <MY_NAME@example.com>
2021-06-30 20:34:59 +0300

    scroll animation

--- js/main.js
+++ js/main.js
@@ -1,23 +1,62 @@
 const backToTopDOM = document.querySelector('.back-to-top');
 const stickyDOM = document.querySelector('header')
+let wasStickyAtLeastOcne = false;
+let notStickyTimer = null;
+let lastScrollYPosition = window.scrollY;
+let currentScrollYPosition = 0;
 
-//naudojamas ivykio tipas - scroll
-//sukuriame ivyki visai narsyklei(window), kuris nurodys elemento pozicija joje
-addEventListener('scroll', () => {
-    // window.scrollY/window.scrollX nurodo pozicija narsykleje
-    if (window.scrollY > 500) {
-        backToTopDOM.classList.remove('hide')
 
+function contentScroll() {
+    currentScrollYPosition = window.scrollY;
+    console.log(currentScrollYPosition);
+
+    // back to top
+    if (currentScrollYPosition > 500) {
+        backToTopDOM.classList.remove('hide');
     } else {
         backToTopDOM.classList.add('hide');
     }
-    if (window.scrollY > 150) {
-        stickyDOM.classList.add('sticky');
-    } else {
-        stickyDOM.classList.remove('sticky');
+
+    // kai leidziame i apacia
+    if (currentScrollYPosition > 150 && currentScrollYPosition >= lastScrollYPosition) {
+        wasStickyAtLeastOnce = true;
+        headerDOM.classList.add('sticky');
+        stickyDOM.classList.remove('not-sticky');
     }
 
+    // kai kylame i virsiu
+    if (currentScrollYPosition < 250 && currentScrollYPosition < lastScrollYPosition) {
+        stickyDOM.classList.remove('sticky');
+        if (wasStickyAtLeastOnce) {
+            stickyDOM.classList.add('not-sticky');
+
+            notStickyTimer = setTimeout(() => {
+                stickyDOM.classList.remove('not-sticky');
+                wasStickyAtLeastOnce = false;
+            }, 400)
+        }
+    }
+
+    lastScrollYPosition = currentScrollYPosition;
+}
+
+window.addEventListener('scroll', () => {
+    contentScroll();
 })
+
+contentScroll();
+
+//     // back to top
+//     if (currentScrollYPosition > 500) {
+
+// //naudojamas ivykio tipas - scroll
+// //sukuriame ivyki visai narsyklei(window), kuris nurodys elemento pozicija joje
+// addEventListener('scroll', () => {
+//     // window.scrollY/window.scrollX nurodo pozicija narsykleje
+//     if (window.scrollY > 500) {
+//         backToTopDOM.classList.remove('hide')
+
+
 
 
 
