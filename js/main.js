6b89e11a4e67249d346e2337cb0573502b7365c4
rainverted <MY_NAME@example.com>
2021-06-30 19:10:15 +0300

    back to top scroll funkcionalumas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-console.log('arfdhg');
+const backToTopDOM = document.querySelector('.back-to-top');
+const stickyDOM = document.querySelector('header')
+
+//naudojamas ivykio tipas - scroll
+//sukuriame ivyki visai narsyklei(window), kuris nurodys elemento pozicija joje
+addEventListener('scroll', () => {
+    // window.scrollY/window.scrollX nurodo pozicija narsykleje
+    if (window.scrollY > 500) {
+        backToTopDOM.classList.remove('hide')
+
+    } else {
+        backToTopDOM.classList.add('hide');
+    }
+    if (window.scrollY > 150) {
+        stickyDOM.classList.add('sticky');
+    } else {
+        stickyDOM.classList.remove('sticky');
+    }
+
+})
+
+
+
+
